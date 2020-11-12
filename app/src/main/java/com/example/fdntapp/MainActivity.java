290f104accdaf96dc9215b118b7fa1614f25690d
bdziurczak <bbb05bart@gmail.com>
2020-11-12 10:30:17 +0100

    Using 1 actionbar for each tab, implementing NavHost as a FragmentContainerView instead of fragment

--- app/src/main/java/com/example/fdntapp/MainActivity.java
+++ app/src/main/java/com/example/fdntapp/MainActivity.java
@@ -7,8 +7,10 @@ import com.example.fdntapp.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -21,22 +23,15 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-
-        /*
-        * Passing each menu ID as a set of Ids because each
-        * menu should be considered as top level destinations.
-        */
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_login, R.id.navigation_abfdnt)
                 .build();
-        /*
-        * Navigating to a destination is done using a NavController, an object that manages app navigation within a NavHost.
-        * Each NavHost has its own corresponding NavController.
-        * NavHost is an empty container where destinations are swapped in and out as a user navigates through your app.
-        * NavHostFragment is an default NavHost implementation
-        */
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // i had to this because i used FragmentContainerView instead of fragment to implement navhost
+        // https://issuetracker.google.com/issues/142847973
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        //
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
