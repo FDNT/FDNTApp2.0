b003a3cc5383b226274b4d88435b5bd998b4c825
bdziurczak <bbb05bart@gmail.com>
2021-01-30 15:58:15 +0100

    Added expand/collapse icon to expandable TextView inside PatronFragment

--- app/src/main/java/com/example/fdntapp/ui/abfdnt/PatronFragment.java
+++ app/src/main/java/com/example/fdntapp/ui/abfdnt/PatronFragment.java
@@ -18,27 +18,29 @@ import org.jetbrains.annotations.NotNull;
 public class PatronFragment extends Fragment {
     private final int COLLAPSED_MAX_LINES = 4;
     FragmentPatronBinding binding;
+    //
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPatronBinding.inflate(inflater, container, false);
         binding.textView.setMaxLines(COLLAPSED_MAX_LINES);
-        binding.textView.setOnClickListener(v -> TextViewTransformation((TextView) v));
+        binding.expandCollapseIcon.setOnClickListener(v -> TextViewTransformation(binding.textView));
         return binding.getRoot();
     }
 
     private void TextViewTransformation(TextView textView) {
         int duration = (binding.textView.getLineCount() - COLLAPSED_MAX_LINES) * 10;
         if (isCollapsed(textView)) {
-
             ObjectAnimator animator = ObjectAnimator.ofInt(binding.textView, "maxLines",
                     binding.textView.getLineCount());
             animator.setDuration(duration).start();
+            binding.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_less_24);
         }
         else {
             ObjectAnimator animator = ObjectAnimator.ofInt(binding.textView, "maxLines",
                     COLLAPSED_MAX_LINES);
             animator.setDuration(duration).start();
+            binding.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_more_24);
         }
     }
 
