4b18d0e6337c3f2988edf6f6cb9f07880192f67a
bdziurczak <bbb05bart@gmail.com>
2021-02-12 19:25:56 +0100

    Implemented PatronFragment

--- app/src/main/java/com/example/fdntapp/ui/abfdnt/PatronFragment.java
+++ app/src/main/java/com/example/fdntapp/ui/abfdnt/PatronFragment.java
@@ -14,6 +14,7 @@ import com.example.fdntapp.R;
 import com.example.fdntapp.databinding.FragmentPatronBinding;
 
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
 
 public class PatronFragment extends Fragment {
     private final int COLLAPSED_MAX_LINES = 4;
@@ -23,28 +24,33 @@ public class PatronFragment extends Fragment {
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPatronBinding.inflate(inflater, container, false);
-        binding.textView.setMaxLines(COLLAPSED_MAX_LINES);
-        binding.expandCollapseIcon.setOnClickListener(v -> TextViewTransformation(binding.textView));
+        setClickListener();
         return binding.getRoot();
+
+    }
+
+    private void setClickListener() {
+        binding.expandable.text.setOnClickListener(v -> TextViewTransformation(binding.expandable.text));
+        binding.expandable.expandCollapseIcon.setOnClickListener(v -> TextViewTransformation(binding.expandable.text));
     }
 
     private void TextViewTransformation(TextView textView) {
-        int duration = (binding.textView.getLineCount() - COLLAPSED_MAX_LINES) * 10;
+        int duration = (binding.expandable.text.getLineCount() - COLLAPSED_MAX_LINES) * 10;
         if (isCollapsed(textView)) {
-            ObjectAnimator animator = ObjectAnimator.ofInt(binding.textView, "maxLines",
-                    binding.textView.getLineCount());
+            ObjectAnimator animator = ObjectAnimator.ofInt(binding.expandable.text, "maxLines",
+                    binding.expandable.text.getLineCount());
             animator.setDuration(duration).start();
-            binding.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_less_24);
+            binding.expandable.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_less_24);
         }
         else {
-            ObjectAnimator animator = ObjectAnimator.ofInt(binding.textView, "maxLines",
+            ObjectAnimator animator = ObjectAnimator.ofInt(binding.expandable.text, "maxLines",
                     COLLAPSED_MAX_LINES);
             animator.setDuration(duration).start();
-            binding.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_more_24);
+            binding.expandable.expandCollapseIcon.setImageResource(R.drawable.ic_baseline_expand_more_24);
         }
     }
-
     private boolean isCollapsed(TextView textView) {
-        return textView.getMaxLines() == COLLAPSED_MAX_LINES;
+        return  textView.getMaxLines() == COLLAPSED_MAX_LINES;
     }
+
 }
